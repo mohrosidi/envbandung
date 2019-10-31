a5ba8591feed6a867c2776c5e2ff7acaaea707b4
mohrosidi <48061153+mohrosidi@users.noreply.github.com>
2019-10-31 17:16:55 +0700

    menambahkan data baru

--- R/DATASET.R
+++ R/DATASET.R
@@ -2,7 +2,7 @@
 #' @description Dataset ini berisi data kualitas air sungai di Kota Bandung.
 #' @format Tibble dengan 56 baris dan 5 variabel:
 #' \describe{
-#'   \item{\code{No}}{}
+#'   \item{\code{No}}{ }
 #'   \item{\code{Sungai}}{nama sungai}
 #'   \item{\code{Storet_Score}}{nilai indeks STORET}
 #'   \item{\code{Golongan}}{golongan kelas sungai berdasarkan nilai indeks STORET}
@@ -15,7 +15,7 @@
 #' @description Dataset ini berisi data kegiatan sosialisasi KangPisMan Kota Bandung dari tahun 2018 sampai 2019.
 #' @format Tibble dengan 27 baris dan 5 variabel:
 #' \describe{
-#'   \item{\code{No}}{}
+#'   \item{\code{No}}{ }
 #'   \item{\code{Tanggal}}{tanggal pelaksanaan sosialisasi}
 #'   \item{\code{Kegiatan}}{nama kegiatan}
 #'   \item{\code{Narasumber}}{narasumber kegiatan sosialisasi}
@@ -58,3 +58,76 @@
 #' @source \url{http://data.bandung.go.id/dataset/luas-taman-kota-di-wilayah-kota-bandung}
 "taman"
 
+#' @title Lokasi Tempat Pembuangan Sampah Kota Bandung
+#' @description Dataset ini berisi data tempat pembuangan sampah kota Bandung.
+#' @format Tibble dengan 159 baris dan 11 variabel:
+#' \describe{
+#'   \item{\code{No}}{ }
+#'   \item{\code{Wilayah}}{nama wilayah layanan}
+#'   \item{\code{Kecamatan}}{nama kecamatan}
+#'   \item{\code{TPS}}{nama tps}
+#'   \item{\code{Lokasi}}{lokasi tps}
+#'   \item{\code{Sampah_masuk}}{volume sampah masuk (m3/hari)}
+#'   \item{\code{Sampah_diangkut}}{volume sampah diangkut ke tpa (m3/hari)}
+#'   \item{\code{Ritasi}}{jumlah ritasi}
+#'   \item{\code{Peruntukkan}}{peruntukan tps}
+#'   \item{\code{Jadwal_pengangkutan}}{jadwal pengangkutan}
+#'   \item{\code{Sumber_sampah}}{sumber sampah yang masuk ke tps}
+#'}
+#' @source \url{http://data.bandung.go.id/dataset/lokasi-tempat-pembuangan-sampah-kota-bandung}
+"tps"
+
+#' @title Bendungan di Kota Bandung
+#' @description Dataset ini berisi data bendungan di Kota Bandung
+#' @format Tibble dengan 42 baris dan 6 variabel:
+#' \describe{
+#'   \item{\code{No}}{ }
+#'   \item{\code{Bendungan}}{nama bendungan}
+#'   \item{\code{Sumber_air}}{sumber air bendungan}
+#'   \item{\code{Kelurahan}}{lokasi kelurahan bendungan}
+#'   \item{\code{Kecamatan}}{lokasi kecamatan bendungan}
+#'   \item{\code{Keterangan}}{keterangan terkait pemanfaatan bendungan}
+#'}
+#' @source \url{http://data.bandung.go.id/dataset/bendungan-di-kota-bandung}
+"bendungan"
+
+#' @title Sumber Mata Air di Kota Bandung
+#' @description Dataset ini berisi data sumber mata air di Kota Bandung.
+#' @format Tibble dengan 82 baris 9 variabel:
+#' \describe{
+#'   \item{\code{No}}{ }
+#'   \item{\code{Wilayah}}{nama wilayah}
+#'   \item{\code{Mata_air}}{nama mata air}
+#'   \item{\code{Saluran}}{saluran/sungai lokasi air mata air mengalir}
+#'   \item{\code{Kampung}}{nama kampung mata air}
+#'   \item{\code{Kelurahan}}{nama kelurahan}
+#'   \item{\code{Kecamatan}}{nama kecamatan}
+#'   \item{\code{Debit}}{debit rata-rata (l/detik)}
+#'   \item{\code{Penggunaan}}{pemanfaatan mata air}
+#'   \item{\code{Keterangan}}{keterangan kepemilikan lahan mata air}
+#'}
+#' @source \url{http://data.bandung.go.id/dataset/sumber-mata-air-di-kota-bandung}
+"mata_air"
+
+#' @title Panjang Anak Sungai Citarum di Kota Bandung
+#' @description Dataset ini berisi data panjang anak sungai citarum yang terdiri dari 15 anak sungai.
+#' @format Tibble dengan 15 baris dan 2 variabel:
+#' \describe{
+#'   \item{\code{anak_sungai}}{nama anak sungai}
+#'   \item{\code{Panjang}}{panjang anak sungai (km)}
+#'}
+#' @source \url{http://data.bandung.go.id/dataset/panjang-anak-sungai-citarum-di-kota-bandung}
+"anak_sungai"
+
+#' @title Jumlah Pelanggan Air Minum Menurut Golongan Tarif di Kota Bandung
+#' @description Dataset ini berisi mengenai data jumlah pelanggan air minum menurut golongan tarif di Kota Bandung.
+#' @format Tibble dengan 396 baris dan 4 variabel:
+#' \describe{
+#'   \item{\code{Golongan_tarif}}{golongan tarif}
+#'   \item{\code{Bulan}}{bulan pencatatan}
+#'   \item{\code{Jumlah_pelanggan}}{jumlah pelanggan}
+#'   \item{\code{Tahun}}{tahun pencatatan}
+#'}
+#' @note Golongan tarif : 1A (Sosial Umum), 1B (Sosial Khusus), 2A1 (Rumah Tangga), 2A2 (Rumah Tangga), 2A3 (Rumah Tangga), 2A4 (Rumah Tangga), 2B (Instansi), 3A (Niaga Kecil), 3B (Niaga Menengah/Besar), 4A (Industri Kecil), 4B (Industri Menengah/Besar)
+#' @source \url{http://data.bandung.go.id/dataset/jumlah-pelanggan-air-minum-menurut-golongan-tarif-di-kota-bandung}
+"pelanggan_am"
--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -23,7 +23,44 @@ bank_sampah <- read_csv("http://data.bandung.go.id/dataset/90a91b72-12a7-4a3d-b1
 taman <- read_csv("http://data.bandung.go.id/dataset/057b0e8c-3597-4818-a376-e239ae10c65f/resource/bbdd95c9-3ca8-47e7-ba24-4746276011eb/download/data-luas-taman-di-sub-wilayah-kota-bandung-tahun-2018.csv") %>%
   select(Wilayah, Luas = `Luas (m2)`, Jumlah = `Jumlah Taman`)
 
+tps <- read_csv("http://data.bandung.go.id/dataset/9f6ef681-6d4e-491c-8db7-86ba61ea517d/resource/654fc9f8-5851-4c44-8138-31c7e89adbf9/download/data-tps-kota-bandung-2018.csv") %>%
+  select(No = No., Wilayah, Kecamatan, TPS = `Nama TPS`, Lokasi = `Lokasi TPS (Jalan)`,
+         Sampah_masuk = `Sampah Yang Masuk (M3/hari)`, Sampah_diangkut = `Sampah Diangkut ke TPA (M3/hari)`,
+         Ritasi, Peruntukkan, Jadwal_pengangkutan = `Jadwal Pengangkutan`, Sumber_sampah=`Sumber Sampah (RW - Kelurahan - Kecamatan)`)
+
+bendungan <- read_csv("http://data.bandung.go.id/dataset/9d2a9857-d828-489a-ba9c-2784aaa3e921/resource/93f9ec1b-9cdc-4fb9-8b98-a5d590ec0639/download/data-bendungan---tahun-2008.csv") %>%
+  select(No, Bendungan = `Nama Bendungan`, Sumber_air = `Sumber Air`, Kelurahan, Kecamatan, Keterangan) %>%
+  mutate(Keterangan = ifelse(Keterangan=='-', NA, Keterangan))
+
+mata_air <- read_csv("http://data.bandung.go.id/dataset/995b8ac6-05ce-4b57-a504-fc6492fcab28/resource/544d8cee-6ec5-4981-a401-4e685658b5bc/download/data-sumber-mata-air---tahun-2008.csv") %>%
+  select(No, Wilayah, Mata_air = `Nama Mata Air`, Saluran = `Saluran/Sungai`,
+         Kampung, Kelurahan, Kecamatan, Debit = `Debit Rata-rata I / det`,
+         Penggunaan, Keterangan)
+
+anak_sungai <- read_csv("http://data.bandung.go.id/dataset/22e9c22b-124f-4a58-9969-f1c652a66654/resource/ff548d54-c339-48a8-a2ac-6bffd87bb79e/download/data-panjang-anak-sungai-citarum-di-kota-bandung-2017.csv") %>%
+  select(anak_sungai = `Nama Anak Sungai`, Panjang = `Panjang (km)`)
+
+am1 <- read_csv("http://data.bandung.go.id/dataset/ac8e7947-9f98-458b-be51-06fa78cf427c/resource/0fd94759-ac74-4c2a-afec-07340ccdcb57/download/jumlah-pelanggan-air-minum-berdasarkan-golongan-tarif-2015.csv") %>%
+  select(-X14 ) %>%
+  rename(Golongan_tarif = `Golongan Tarif`) %>%
+  mutate_all(function(x){ifelse(x=='-', NA, x)}) %>%
+  gather(key="Bulan", value="Jumlah_pelanggan",-Golongan_tarif) %>%
+  mutate(Jumlah_pelanggan=as.numeric(Jumlah_pelanggan), Tahun=2015)
+am2 <- read_csv("http://data.bandung.go.id/dataset/ac8e7947-9f98-458b-be51-06fa78cf427c/resource/24285a01-14ba-4dcc-8559-8d077fdf040d/download/jumlah-pelanggan-air-minum-berdasarkan-golongan-tarif-2016.csv") %>%
+  rename(Golongan_tarif = `Golongan Tarif`)  %>%
+  gather(key="Bulan", value="Jumlah_pelanggan",-Golongan_tarif, convert = TRUE) %>%
+  mutate(Tahun=2016)
+am3 <- read_csv("http://data.bandung.go.id/dataset/ac8e7947-9f98-458b-be51-06fa78cf427c/resource/9e18c867-e578-4eb8-b5b6-4e996e79f2c4/download/jumlah-pelanggan-air-minum-berdasarkan-golongan-tarif-2017.csv") %>%
+  rename(Golongan_tarif = `Golongan Tarif`, Jumlah_pelanggan = `Jumlah Pelanggan`) %>%
+  mutate(Bulan = "Desember", Tahun = 2017) %>%
+  select(Golongan_tarif, Bulan, Jumlah_pelanggan, Tahun)
+am4 <- read_csv("http://data.bandung.go.id/dataset/ac8e7947-9f98-458b-be51-06fa78cf427c/resource/4ab7cf37-8c09-4829-a59b-3ead0bb5f429/download/jumlah-pelanggan-air-minum-berdasarkan-golongan-tarif-2018.csv") %>%
+  select(-Agustus_1) %>%
+  rename(Golongan_tarif=`Golongan Tarif`)  %>%
+  gather(key="Bulan", value="Jumlah_pelanggan",-Golongan_tarif, convert = TRUE) %>%
+  mutate(Tahun=2018)
+pelanggan_am <- bind_rows(am1, am2, am3, am4)
 
 usethis::use_data(kual_sungai, KangPisMan, mitra_KangPisMan, bank_sampah,
-                  taman,
+                  taman, tps, bendungan, mata_air, anak_sungai, pelanggan_am,
                   overwrite = TRUE)
